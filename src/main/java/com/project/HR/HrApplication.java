92735a7be8be6210b71b6568dd62a99280010c1a
amber <a0003121@hotmail.com>
2022-07-17 12:30:51 +0800

    update heroku setting

--- src/main/java/com/project/HR/HrApplication.java
+++ src/main/java/com/project/HR/HrApplication.java
@@ -2,11 +2,12 @@ package com.project.HR;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling //允許排程
-public class HrApplication {
+public class HrApplication extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HrApplication.class, args);
